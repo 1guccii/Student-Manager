24a21ba32e24b90078abd8c44cac280eb683b7bd
Duc Hoang Manh <149278126+Jandyz-1@users.noreply.github.com>
2024-10-02 14:54:40 +0700

    feat : integrate bottom app bar

---
+++ app/src/main/java/com/j4kerstudio/studentmanager/data/model/Student.kt
@@ -0,0 +1,9 @@
+package com.j4kerstudio.studentmanager.data.model
+
+data class Student(
+    val idStudent: String,
+    val idClass : String,
+    val mathematicsGrade : Double,
+    val physicGrade : Double,
+    val chemistryGrade : Double
+)
